af5ed29b4f99f888611709d0e612cdc3ac4bbc63
medertynychbekov <meder.tynychbekov@gmail.com>
2023-12-30 20:58:06 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String name = "Meder"; // M e d e r
+
+        String text = "I love you ";
+
+        System.out.println(text.concat(name));
+
+    }
+
+}
